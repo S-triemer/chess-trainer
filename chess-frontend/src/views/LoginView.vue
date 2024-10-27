6bb5cd2f559089fba024f4d484bc593ecbe1fb56
Sebastian Triemer <sebastian.triemer@outlook.de>
2024-10-27 06:33:11 +0100

    basic login works

---
+++ chess-frontend/src/views/LoginView.vue
@@ -0,0 +1,32 @@
+<script setup>
+import axios from 'axios'
+import { ref } from 'vue'
+
+const username = ref('')
+const password = ref('')
+
+async function login() {
+  try {
+    const response = await axios.post('http://127.0.0.1:81/api/users/login', {
+      username: username.value,
+      password: password.value
+    })
+    console.log(response)
+    //const token = response.data.token
+    //localStorage.setItem('jwt', token) // Store JWT
+    //this.$router.push('/user') // Redirect to user page
+    console.log('logged in')
+  } catch (error) {
+    console.error('Login failed:', error)
+    // Handle error (e.g., show message)
+  }
+}
+</script>
+
+<template>
+  <form @submit.prevent="login">
+    <input v-model="username" placeholder="Username" required />
+    <input v-model="password" type="password" placeholder="Password" required />
+    <button type="submit">Login</button>
+  </form>
+</template>
